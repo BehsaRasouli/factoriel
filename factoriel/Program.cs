b486e5af3cb9aaf7220438833d265ff324907bf3
Behsa Rasouli <rasoolibehsa@gmail.com>
2025-12-11 00:12:15 +0400

    Add project files.

---
+++ factoriel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static int facto(int n)
+        {
+
+            int mul = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                mul *= i;
+            }
+            return mul;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(facto(4));
+
+        }
+    }
+}
